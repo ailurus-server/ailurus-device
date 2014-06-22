c4897129d4d89bd76a14984df766e5c5bb3bea13
Tian Yu Zhang <tianyu2357@gmail.com>
2014-06-22 19:15:45 -0400

    add undertow hello world app

---
+++ undertow/src/main/java/tk/ailurus/boss/Boss.java
@@ -0,0 +1,22 @@
+package tk.ailurus.boss;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class Boss {
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "0.0.0.0")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+
+        server.start();
+    }
+}
