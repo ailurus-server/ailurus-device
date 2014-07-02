6f0179bcc53599c15e6d6069c39d713c0d6af895
Tian Yu Zhang <tianyu2357@gmail.com>
2014-07-02 05:09:48 -0400

    remove jsp support
    Summary: remove jsp support
    
    Test Plan: NA
    
    Reviewers: rpeng
    
    Reviewed By: rpeng
    
    Differential Revision: http://216.58.1.115:9000/D25

--- Boss/src/main/java/ca/ailurus/boss/server/WelcomeServlet.java
+++ Boss/src/main/java/ca/ailurus/boss/server/WelcomeServlet.java
@@ -7,32 +7,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class WelcomeServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/index.jsp");
 
         boolean isInitialized = false;
         try (Storage storage = new Storage()) {
             isInitialized = storage.isInitialized();
         }
 
-        request.setAttribute("isInitialized", isInitialized);
-        if (!isInitialized) {
-            dispatcher.forward(request, response);
-            return;
-        }
-
-        String userId = null;
-
-        HttpSession session = request.getSession();
-        synchronized (session) {
-            userId = (String) session.getAttribute("userId");
-        }
-
-        request.setAttribute("userId", userId);
-        dispatcher.forward(request, response);
+        PrintWriter writer = response.getWriter();
+        writer.print(
+                "<!DOCTYPE html>\n" +
+                "<html class=\"expand\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Ailurus Boss</title>\n" +
+                "    <script src=\"boss/boss.nocache.js\"></script>\n" +
+                "    <script>\n" +
+                "        var isInitialized = " + isInitialized + ";\n" +
+                "    </script>\n" +
+                "</head>\n" +
+                "<body class=\"expand\">\n" +
+                "<iframe src=\"javascript:''\" id=\"__gwt_historyFrame\" tabIndex='-1' style=\"position:absolute;width:0;height:0;border:0\"></iframe>\n" +
+                "    <noscript>\n" +
+                "        <div style=\"width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; border: 1px solid red; padding: 4px; font-family: sans-serif\">\n" +
+                "            Your web browser must have JavaScript enabled\n" +
+                "            in order for this application to display correctly.\n" +
+                "        </div>\n" +
+                "    </noscript>\n" +
+                "</body>\n" +
+                "</html>");
     }
 }
